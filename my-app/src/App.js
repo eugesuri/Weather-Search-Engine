1a832cede1c80e1ad06f6d4f55b97ceb5c54866a
eugesuri <140730354+eugesuri@users.noreply.github.com>
2023-11-15 16:06:43 +0100

    added axios

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <footer>
+        This project was coded by <em> Eugenia Brunetti Ehnimb</em>
+        {""} and is open sourced on {""}
+        <a href="https://github.com/eugesuri/Weather-React-4" target="_blank">
+          Github{" "}
+        </a>
+        🌸
+      </footer>
+    </div>
+  );
+}
